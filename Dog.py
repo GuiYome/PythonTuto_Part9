150df9f4a605909ea1a96141ceb2a0c675d97437
Guillaume Huby <guillaume.huby@geosys.com>
2018-05-04 20:01:23 +0200

    Step 9.3.5: Add tricks attribute and method

--- Dog.py
+++ Dog.py
@@ -2,5 +2,10 @@ class Dog:
 
     kind = 'canine'         # class variable shared by all instances
 
+    tricks = []
+
     def __init__(self, name):
         self.name = name    # instance variable unique to each instance
+
+    def add_trick(self, trick):
+        self.tricks.append(trick)
